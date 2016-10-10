9ae9e06808ad60c82ce7ddd2c9f784c909eceb7c
jps <me@jspenc.com>
2016-10-10 20:31:36 +0100

    triangle

--- OpenTkPlayground/Program.cs
+++ OpenTkPlayground/Program.cs
@@ -1,4 +1,6 @@
-﻿using OpenTK;
+﻿using System.IO;
+using OpenTkPlayground.Uniforms;
+using OpenTK;
 using OpenTK.Graphics;
 using OpenTK.Graphics.OpenGL;
 
@@ -7,14 +9,47 @@ namespace OpenTkPlayground
     internal sealed class Program
     {
         public static void Main(string[] args)
-        {
-
+        {            
             using (var win = new GameWindow(1280, 720, GraphicsMode.Default, "OpenTK Intro",
                 GameWindowFlags.Default, DisplayDevice.Default,
                 // ask for an OpenGL 3.0 forward compatible context
                 3, 0, GraphicsContextFlags.ForwardCompatible))
             {
-                win.Load += (s, e) => { /* init global GL resources here */ };
+                VertexBuffer<ColouredVertex> vertexBuffer = null;
+                ShaderProgram shaderProgram = null;
+                VertexArray<ColouredVertex> vertexArray = null;
+                Matrix4Uniform projectionMatrix = null; 
+
+                win.Load += (s, e) =>
+                {
+                    // create and fill a vertex buffer
+                    vertexBuffer = new VertexBuffer<ColouredVertex>(ColouredVertex.Size);
+                    vertexBuffer.AddVertex(new ColouredVertex(new Vector3(-1, -1, -1.5f), Color4.Lime));
+                    vertexBuffer.AddVertex(new ColouredVertex(new Vector3(1, 1, -1.5f), Color4.Red));
+                    vertexBuffer.AddVertex(new ColouredVertex(new Vector3(1, -1, -1.5f), Color4.Blue));
+
+                    var vertexShader = new Shader(ShaderType.VertexShader,
+                        File.ReadAllText(@"../../VertexShaders/vertex-shader.vs"));
+                    var fragmentShader = new Shader(ShaderType.FragmentShader,
+                        File.ReadAllText(@"../../FragmentShaders/fragment-shader.fs"));
+
+                    // link shaders into shader program
+                    shaderProgram = new ShaderProgram(vertexShader, fragmentShader);
+
+                    // create vertex array to specify vertex layout
+                    vertexArray = new VertexArray<ColouredVertex>(
+                        vertexBuffer, shaderProgram,
+                        new VertexAttribute("vPosition", 3, VertexAttribPointerType.Float, ColouredVertex.Size, 0),
+                        new VertexAttribute("vColor", 4, VertexAttribPointerType.Float, ColouredVertex.Size, 12)
+                        );
+
+                    // create projection matrix uniform
+                    projectionMatrix = new Matrix4Uniform("projectionMatrix")
+                    {
+                        Matrix = Matrix4.CreatePerspectiveFieldOfView(MathHelper.PiOver2, 16f/9, 0.1f, 100f)
+                    };
+
+                };
                 win.Unload += (s, e) => { /* dispose global GL resources here */ };
                 win.RenderFrame += (s, e) =>
                 {
@@ -23,6 +58,24 @@ namespace OpenTkPlayground
 
                     //http://genericgamedev.com/tutorials/opengl-in-csharp-an-object-oriented-introduction-to-opentk/4/
 
+                    // activate shader program and set uniforms
+                    shaderProgram.Use();
+                    projectionMatrix.Set(shaderProgram);
+
+                    // bind vertex buffer and array objects
+                    vertexBuffer.Bind();
+                    vertexArray.Bind();
+
+                    // upload vertices to GPU and draw them
+                    vertexBuffer.BufferData();
+                    vertexBuffer.Draw();
+
+                    // reset state for potential further draw calls (optional, but good practice)
+                    GL.BindVertexArray(0);
+                    GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+                    GL.UseProgram(0);
+
+                    // swap backbuffer
                     win.SwapBuffers();
                 };
                 win.Resize += (s, e) =>
